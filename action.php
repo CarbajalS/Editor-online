17298a6aaacc187c7b298906bd04ba0502167803
CarbajalS <38165890+CarbajalS@users.noreply.github.com>
2018-08-07 00:04:41 -0500

    Update action.php

--- action.php
+++ action.php
@@ -1,9 +1,18 @@
-<?php
-
-if(isset($_REQUEST)){
-  $env=$_REQUEST['nome'];
-// Criando um novo arquivo
-file_put_contents("novo.php", $env);}
-echo file_get_contents("novo.php");
-
-?>
+<?php
+
+
+
+if(isset($_REQUEST)){
+
+  $env=$_REQUEST['nome'];
+
+// Criando um novo arquivo
+
+file_put_contents("novo.html", $env);}
+
+echo file_get_contents("novo.html");
+
+
+
+?>
+
